ed9453ce857c5fb2ec0630472053b740e4a023dd
Tolo <iv1055@yandex.ru>
2022-07-10 20:03:40 +0000

    MediaStore

--- app/src/main/java/com/example/videoplayer3/MainActivity.java
+++ app/src/main/java/com/example/videoplayer3/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import java.util.ArrayList;
 
@@ -22,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ArrayList<VideoModel> fetchAllVideos(Context context) {
+        ArrayList<VideoModel> videoModels = new ArrayList<>();
 
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        String orderBy = MediaStore.Video.Media.DATE_ADDED + " DESC";
+
+        String[] projection = {
+                MediaStore.Video.Media._ID,
+        MediaStore.Video.Media.DATA,
+        MediaStore.Video.Media.SIZE,
+        MediaStore.Video.Media.TITLE,
+        MediaStore.Video.Media.HEIGHT,
+        MediaStore.Video.Media.DURATION,
+        MediaStore.Video.Media.DISPLAY_NAME,
+                MediaStore.Video.Media.RESOLUTION,
+
+        }
     }
 }
